6008a9598a8ae30f48b5b463f0de88d5a00cdcac
anastasiatol <tolstykoanastasia@gmail.com>
2017-07-30 18:23:23 +0300

    Add loading images from server

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-console.log("hello");
+(function () {
+    function getImagesFromServer() {
+        return [{
+            src: "img/food4.jpg"
+        }, {
+            src: "img/food4.jpg"
+        }, {
+            src: "img/food4.jpg"
+        }, {
+            src: "img/food4.jpg"
+        }];
+    }
+
+    function renderGallery() {
+        var images = getImagesFromServer();
+
+        images.forEach(function (image) {
+            var img = document.createElement("img");
+            img.src = image.src;
+            $(img).addClass("small");
+
+            $("#gallery").append(img);
+        });
+    }
+
+
+
+    $(document).ready(function () {
+        renderGallery();
+    });
+})();
