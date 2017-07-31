7f834c34977fdcb2b2bea9666058a7a11313cf4b
anastasiatol <tolstykoanastasia@gmail.com>
2017-07-31 06:05:59 +0300

    some changes in style

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+(function () {
+    function getImagesFromServer() {
+        return ["img/food4.jpg", "img/food4.jpg", "img/food4.jpg", "img/food4.jpg"];
+    }
+
+    function renderGallery() {
+        var images = getImagesFromServer();
+
+        images.forEach(function (image) {
+            var img = document.createElement("img");
+            img.src = image.src;
+            $(img).addClass("small");
+
+            $("#gallery").append(img);
+        });
+    }
+
+
+
+    $(document).ready(function () {
+        renderGallery();
+    });
+})();
